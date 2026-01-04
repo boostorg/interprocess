e7bc8308869bc4c7eb0f4c3f4f2825d14b8f93c9
Ion Gaztañaga <igaztanaga@gmail.com>
2026-01-04 15:43:56 +0100

    #252 (['"[regression] no viable constructor for offset_ptr with clang from 1.87.0"])].

--- include/boost/interprocess/offset_ptr.hpp
+++ include/boost/interprocess/offset_ptr.hpp
@@ -251,6 +251,25 @@ namespace ipcdetail {
                    , Ret>
    {};
 
+   template <class T, class P>
+   struct is_ptr_constructible;
+
+   template <class T, class P>
+   struct is_ptr_constructible<T*, P*>
+   {
+      private:
+      template<class U> static U get();
+
+      template <typename U>
+      static yes_type test( typename enable_if_c< sizeof( new U*(get<P*>()) ) != 0, int >::type );
+         
+      template <typename U>
+      static no_type test(...);
+         
+      public:
+      static const bool value = sizeof(test<T>(0)) == sizeof(yes_type);
+   };
+
 }  //namespace ipcdetail {
 #endif   //#ifndef BOOST_INTERPROCESS_DOXYGEN_INVOKED
 
@@ -353,6 +372,20 @@ class offset_ptr
    template<class T2>
    BOOST_INTERPROCESS_FORCEINLINE offset_ptr( const offset_ptr<T2, DifferenceType, OffsetType, OffsetAlignment> &ptr
              , typename ipcdetail::enable_if_convertible_unequal_address<T2, PointedType>::type* = 0) BOOST_NOEXCEPT
+      : internal(ipcdetail::offset_ptr_to_offset<OffsetType>(static_cast<PointedType*>(ptr.get()), this))
+   {}
+
+   //!Constructor from other offset_ptr. Only takes part in overload resolution
+   //!if PointedType* is constructible from T2* other than via a conversion (e.g. cast to a derived class). Never throws.
+   template<class T2>
+   BOOST_INTERPROCESS_FORCEINLINE explicit offset_ptr(const offset_ptr<T2, DifferenceType, OffsetType, OffsetAlignment> &ptr
+             #ifndef BOOST_INTERPROCESS_DOXYGEN_INVOKED
+             , typename ipcdetail::enable_if_c<
+                                                !::boost::move_detail::is_convertible<T2*, PointedType*>::value &&
+                                                ipcdetail::is_ptr_constructible<T2*, PointedType*>::value
+                                              >::type * = 0
+             #endif
+             ) BOOST_NOEXCEPT
       : internal(ipcdetail::offset_ptr_to_offset<OffsetType>(static_cast<PointedType*>(ptr.get()), this))
    {}
 
